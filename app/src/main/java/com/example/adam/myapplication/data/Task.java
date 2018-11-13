e18d12dac3fd0361b47b581787de5d9964883dbe
Adam Jackowski <adamus95@vp.pl>
2018-11-13 18:36:04 +0100

    second

---
+++ app/src/main/java/com/example/adam/myapplication/data/Task.java
@@ -0,0 +1,69 @@
+package com.example.adam.myapplication.data;
+
+public class Task {
+    private String date;
+
+    public String getHour() {
+        return hour;
+    }
+
+    public void setHour(String hour) {
+        this.hour = hour;
+    }
+
+    private String hour;
+    private String type;
+    private boolean status;
+    private String result;
+    private double dose;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public double getDose() {
+        return dose;
+    }
+
+    public void setDose(double dose) {
+        this.dose = dose;
+    }
+
+    public Task(String type, String date)
+    {
+        this.type = type;
+        this.date = date;
+        this.status = false;
+        this.result = "empty";
+        this.dose = -1;
+
+    }
+}
