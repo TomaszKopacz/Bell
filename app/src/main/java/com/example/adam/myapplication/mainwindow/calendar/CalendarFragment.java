25b311e04723f52221022a5048f994a4c4e2b741
TomaszKopacz <tomasz.kopacz@onet.pl>
2018-11-26 22:31:55 +0100

    Calendar - view/presenter

--- app/src/main/java/com/example/adam/myapplication/mainwindow/calendar/CalendarFragment.java
+++ app/src/main/java/com/example/adam/myapplication/mainwindow/calendar/CalendarFragment.java
@@ -2,12 +2,9 @@ package com.example.adam.myapplication.mainwindow.calendar;
 
 import android.app.Activity;
 import android.app.Fragment;
-import android.arch.lifecycle.LifecycleOwner;
-import android.arch.lifecycle.Observer;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,15 +13,8 @@ import com.applandeo.materialcalendarview.CalendarView;
 import com.applandeo.materialcalendarview.EventDay;
 import com.example.adam.myapplication.R;
 import com.example.adam.myapplication.app.App;
-import com.example.adam.myapplication.data.Task;
-import com.example.adam.myapplication.data.TaskRepository;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
 public class CalendarFragment extends Fragment {
 
@@ -55,43 +45,17 @@ public class CalendarFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        initializeTasks();
+        DatePickerPresenterImpl presenter = new DatePickerPresenterImpl(this,
+                ((App)activity.getApplication()).getTaskRepository());
+        presenter.initializeTasks();
     }
 
-    public void initializeTasks() {
-
-        if (activity != null) {
-            TaskRepository repository = ((App) activity.getApplication()).getTaskRepository();
-            repository.gatAll().observe((LifecycleOwner) activity, new Observer<List<Task>>() {
-                @Override
-                public void onChanged(@Nullable List<Task> tasks) {
-                    calendarView.setEvents(parseTasksToDayEvents(tasks));
-                }
-            });
-        }
+    @Override
+    public Context getContext() {
+        return activity;
     }
 
-    @NonNull
-    private List<EventDay> parseTasksToDayEvents(List<Task> tasks) {
-        List<EventDay> events = new ArrayList<>();
-
-        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
-
-        try {
-            for (Task task : tasks) {
-                Date date = formatter.parse(task.getDate());
-                Calendar calendar = Calendar.getInstance();
-                calendar.setTime(date);
-                calendar.add(Calendar.MONTH, 1);
-                EventDay event = new EventDay(calendar, R.drawable.bell);
-                events.add(event);
-            }
-
-        } catch (Exception e) {
-            Log.e("TELM", e.getMessage());
-
-        }
-        return events;
+    public void displayTasks(List<EventDay> tasks){
+        calendarView.setEvents(tasks);
     }
-
 }
--- app/src/main/java/com/example/adam/myapplication/mainwindow/calendar/DatePickerPresenterImpl.java
+++ app/src/main/java/com/example/adam/myapplication/mainwindow/calendar/DatePickerPresenterImpl.java
@@ -1,6 +1,65 @@
 package com.example.adam.myapplication.mainwindow.calendar;
 
+import android.arch.lifecycle.LifecycleOwner;
+import android.arch.lifecycle.Observer;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.applandeo.materialcalendarview.EventDay;
+import com.example.adam.myapplication.R;
+import com.example.adam.myapplication.data.Task;
+import com.example.adam.myapplication.data.TaskRepository;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
 public class DatePickerPresenterImpl {
 
+    private CalendarFragment fragment;
+    private TaskRepository repository;
 
+    public DatePickerPresenterImpl(CalendarFragment fragment, TaskRepository repository){
+        this.fragment = fragment;
+        this.repository = repository;
+    }
+
+    void initializeTasks() {
+
+        if (fragment.getContext() != null) {
+            repository.gatAll().observe((LifecycleOwner) fragment.getContext(), new Observer<List<Task>>() {
+                @Override
+                public void onChanged(@Nullable List<Task> tasks) {
+                    fragment.displayTasks(parseTasksToDayEvents(tasks));
+                }
+            });
+        }
+    }
+
+    @NonNull
+    private List<EventDay> parseTasksToDayEvents(List<Task> tasks) {
+        List<EventDay> events = new ArrayList<>();
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+
+        try {
+            for (Task task : tasks) {
+                Date date = formatter.parse(task.getDate());
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(date);
+                calendar.add(Calendar.MONTH, 1);
+                EventDay event = new EventDay(calendar, R.drawable.bell);
+                events.add(event);
+            }
+
+        } catch (Exception e) {
+            Log.e("TELM", e.getMessage());
+
+        }
+        return events;
+    }
 }
