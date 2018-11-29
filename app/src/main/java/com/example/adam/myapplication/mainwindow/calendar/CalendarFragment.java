917e1e4614c622995a735727657f8e78ad702d4a
TomaszKopacz <tomasz.kopacz@onet.pl>
2018-11-29 15:38:32 +0100

    Refactor MVP

--- app/src/main/java/com/example/adam/myapplication/mainwindow/calendar/CalendarFragment.java
+++ app/src/main/java/com/example/adam/myapplication/mainwindow/calendar/CalendarFragment.java
@@ -1,8 +1,9 @@
 package com.example.adam.myapplication.mainwindow.calendar;
 
-import android.app.Activity;
 import android.app.Fragment;
-import android.content.Context;
+import android.arch.lifecycle.LifecycleOwner;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Observer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
@@ -12,12 +13,20 @@ import android.view.ViewGroup;
 import com.applandeo.materialcalendarview.EventDay;
 import com.example.adam.myapplication.R;
 import com.example.adam.myapplication.app.App;
+import com.example.adam.myapplication.data.Task;
+import com.example.adam.myapplication.data.TaskRepository;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
-public class CalendarFragment extends Fragment implements CalendarView {
+public class CalendarFragment extends Fragment implements CalendarContract.CalendarView {
 
-    private Activity activity;
+    private CalendarPresenter presenter;
     private com.applandeo.materialcalendarview.CalendarView calendarView;
 
     public CalendarFragment() {
@@ -25,32 +34,76 @@ public class CalendarFragment extends Fragment implements CalendarView {
     }
 
     @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        this.activity = activity;
-    }
-
-    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.fragment_calendar, container, false);
-        calendarView = view.findViewById(R.id.calendar);
 
-        CalendarPresenterImpl presenter = new CalendarPresenterImpl(this,
-                ((App)activity.getApplication()).getTaskRepository());
-        presenter.presentTasks();
+        getComponents(view);
+        setPresenter();
 
         return view;
     }
 
-    @Override
-    public Context getContext() {
-        return activity;
+    private void getComponents(View view) {
+        calendarView = view.findViewById(R.id.calendar);
+    }
+
+    private void setPresenter(){
+        TaskRepository repository = ((App)getActivity().getApplication()).getTaskRepository();
+        this.presenter = new CalendarPresenter(this, repository);
     }
 
     @Override
-    public void displayTasks(List<EventDay> tasks){
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        presenter.onViewAttached();
+    }
+
+    @Override
+    public void setTasks(LiveData<List<Task>> tasks) {
+        tasks.observe((LifecycleOwner) getActivity(), new Observer<List<Task>>() {
+            @Override
+            public void onChanged(List<Task> tasks) {
+                displayTasks(parseTasksToDayEvents(tasks));
+            }
+        });
+    }
+
+    private void displayTasks(List<EventDay> tasks) {
         calendarView.setEvents(tasks);
+    }
+
+    private List<EventDay> parseTasksToDayEvents(List<Task> tasks) {
+        List<EventDay> events = new ArrayList<>();
+
+        for (Task task : tasks)
+            events.add(convertTaskToDayEvent(task));
+
+        return events;
+    }
+
+    private EventDay convertTaskToDayEvent(Task task) {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+
+        try {
+            Calendar calendar = convertTaskDateToCalendarInstance(task, formatter);
+
+            return new EventDay(calendar, R.drawable.bell);
+
+        } catch (Exception e) {
+
+            return null;
+        }
+    }
+
+    @NonNull
+    private Calendar convertTaskDateToCalendarInstance(Task task, SimpleDateFormat formatter) throws ParseException {
+        Date date = formatter.parse(task.getDate());
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.MONTH, 1);
+
+        return calendar;
     }
 }
---
+++ app/src/main/java/com/example/adam/myapplication/utils/DatetimePicker.java
@@ -0,0 +1,29 @@
+package com.example.adam.myapplication.utils;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
+
+import java.util.Calendar;
+
+public class DatetimePicker {
+
+    public static void showDatePicker(Context context, DatePickerDialog.OnDateSetListener listener) {
+        Calendar calendar = Calendar.getInstance();
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int month = calendar.get(Calendar.MONTH);
+        int year = calendar.get(Calendar.YEAR);
+
+        DatePickerDialog dialog = new DatePickerDialog(context, listener, year, month, day);
+        dialog.show();
+    }
+
+    public static void showTimePicker(Context context, TimePickerDialog.OnTimeSetListener listener){
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+
+        TimePickerDialog dialog = new TimePickerDialog(context, listener, hour, minute, true);
+        dialog.show();
+    }
+}
