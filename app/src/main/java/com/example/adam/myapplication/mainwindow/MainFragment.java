fa01795b7e7fc82503f23fe55f6261055dcb1407
TomaszKopacz <tomasz.kopacz@onet.pl>
2018-12-09 14:52:47 +0100

    Cyclic tasks added

--- app/src/main/java/com/example/adam/myapplication/mainwindow/MainFragment.java
+++ app/src/main/java/com/example/adam/myapplication/mainwindow/MainFragment.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
