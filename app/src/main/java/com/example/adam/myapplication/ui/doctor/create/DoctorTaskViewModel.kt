e9de4e0424bf574416e499a01dbe0d3ccd9c7fd8
TomaszKopacz <tomasz.kopacz@onet.pl>
2019-02-23 19:55:15 +0100

    Doctor task vm ready

--- app/src/main/java/com/example/adam/myapplication/ui/doctor/create/DoctorTaskViewModel.kt
+++ app/src/main/java/com/example/adam/myapplication/ui/doctor/create/DoctorTaskViewModel.kt
@@ -3,29 +3,38 @@ package com.example.adam.myapplication.ui.doctor.create
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.MutableLiveData
-import com.example.adam.myapplication.R
 import com.example.adam.myapplication.data.db.task.TaskRepository
 import com.example.adam.myapplication.data.objects.Doctor
+import com.example.adam.myapplication.data.objects.Task
 import java.util.*
 
 class DoctorTaskViewModel(application: Application) : AndroidViewModel(application) {
 
     private var repository: TaskRepository = TaskRepository(application)
 
+    val task: MutableLiveData<Task?> = MutableLiveData()
+
     val doctor: MutableLiveData<Doctor> = MutableLiveData()
     val date: MutableLiveData<Calendar> = MutableLiveData()
     val endDate: MutableLiveData<Calendar> = MutableLiveData()
     val note: MutableLiveData<String> = MutableLiveData()
 
+    val error: MutableLiveData<String?> = MutableLiveData()
+
+    companion object {
+        private const val NOT_CHOSEN_TEXT = "not chosen"
+        const val IMPROPER_DOCTOR_ERROR = "Doctor not set"
+        const val DATES_ORDER_ERROR = "End date cannot be earlier than date"
+    }
+
     init {
-        setDefaultDoctor(application)
+        setDefaultDoctor()
         setDefaultDates()
     }
 
-    private fun setDefaultDoctor(application: Application) {
-        val emptyDoctor = Doctor()
-        emptyDoctor.specialization = application.resources.getString(R.string.not_chosen_text)
-        doctor.value = emptyDoctor
+    private fun setDefaultDoctor() {
+        doctor.value = Doctor()
+        doctor.value!!.specialization = NOT_CHOSEN_TEXT
     }
 
     private fun setDefaultDates() {
@@ -71,7 +80,41 @@ class DoctorTaskViewModel(application: Application) : AndroidViewModel(applicati
         this.note.value = note
     }
 
-    fun insertTask() {
+    fun submitTask() {
+        try {
+            validateFields()
+            insertTask(newTask())
 
+        } catch (e: Exception) {
+            error.value = e.message
+        }
+    }
+
+    private fun validateFields() {
+        validateDoctor()
+        validateDates()
+    }
+
+    private fun validateDoctor() {
+        if (doctor.value!!.specialization == NOT_CHOSEN_TEXT)
+            throw Exception(IMPROPER_DOCTOR_ERROR)
+    }
+
+    private fun validateDates() {
+        if (endDate.value!!.before(date.value))
+            throw Exception(DATES_ORDER_ERROR)
+    }
+
+    private fun insertTask(newTask: Task) {
+        task.value = newTask
+        repository.insert(task.value)
+    }
+
+    private fun newTask(): Task {
+        val task = Task(Task.DOCTOR, date.value!!.time)
+        task.info = doctor.value!!.specialization
+        task.note = note.value
+
+        return task
     }
 }
